79d6495911fe99685263510f86958691d61ce3b3
lentar-1 <danilo232.2004@ukr.net>
2023-02-13 22:21:48 +0200

    added camera control

---
+++ Be free/Assets/Scripts/Camera/CalculateLerp.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CalculateLerp
+{
+    public static Vector3 CalcLerp(float t, Vector3 P0, Vector3 P1, Vector3 P2, Vector3 P3)
+    {
+        Vector3 seekedPoint =
+            -P0 * Mathf.Pow(t, 3) + 3 * P0 * Mathf.Pow(t, 2) - 3 * P0 * t + P0 +
+            3 * P1 * Mathf.Pow(t, 3) - 6 * P1 * Mathf.Pow(t, 2) + 3 * P1 * t +
+            -3 * P2 * Mathf.Pow(t, 3) + 3 * P2 * Mathf.Pow(t, 2) +
+            P3 * Mathf.Pow(t, 3);
+        return seekedPoint;
+    }
+}
---
+++ Be free/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] private GameObject cam;
+    [SerializeField] private Transform P0;
+    [SerializeField] private Transform P1;
+    [SerializeField] private Transform P2;
+    [SerializeField] private Transform P3;
+    [SerializeField] private Transform lookPoint; 
+    [Range(0f,1f),SerializeField] private float t;
+
+    private void Update()
+    {
+        cam.transform.position = CalculateLerp.CalcLerp(Mathf.Clamp01(t),
+            P0.position, P1.position, P2.position, P3.position);
+        cam.transform.LookAt(lookPoint);
+    }
+}
