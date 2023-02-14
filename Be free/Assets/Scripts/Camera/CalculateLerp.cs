4e7a26dc352732b3719f3f42e40d0e27ec2d4ae0
lentar-1 <danilo232.2004@ukr.net>
2023-02-14 22:05:28 +0200

    camera movement
    smooth movement from a corner to another

--- Be free/Assets/Scripts/Camera/CalculateLerp.cs
+++ Be free/Assets/Scripts/Camera/CalculateLerp.cs
@@ -1,28 +1,34 @@
 //using UnityEngine;
 
-//public class CalculateLerp
-//{
-//    public static Vector3 CalcLerp(float t, Vector3 P0, Vector3 P1, Vector3 P2, Vector3 P3)
-//    {
-//        Vector3 seekedPoint =
-//            -P0 * Mathf.Pow(t, 3) + 3 * P0 * Mathf.Pow(t, 2) - 3 * P0 * t + P0 +
-//            3 * P1 * Mathf.Pow(t, 3) - 6 * P1 * Mathf.Pow(t, 2) + 3 * P1 * t +
-//            -3 * P2 * Mathf.Pow(t, 3) + 3 * P2 * Mathf.Pow(t, 2) +
-//            P3 * Mathf.Pow(t, 3);
-//        return seekedPoint;
-//    }
-//}  для 4 точок
+
 using UnityEngine;
 
 public class CalculateLerp
 {
-    public static Vector3 CalcLerp(float t, Vector3 P0, Vector3 P1, Vector3 P2)
+    public static Vector3 CalcLerp(float t, Vector3 P0, Vector3 P1) // 2 points
+    {
+        Vector3 seekedPoint =
+            (1 - t) * P0 + t * P1;
+        return seekedPoint;
+    }
+    public static Vector3 CalcLerp(float t, Vector3 P0, Vector3 P1, Vector3 P2)// 3 points
     {
         Vector3 seekedPoint =
             (1 - 2*t + t*t) * P0 +
             (t - t * t) * 2 * P1 +
             t * t * P2;
         return seekedPoint;
-    }
-}// для 3 точок
+    }// для 3 точок
+
+    public static Vector3 CalcLerp(float t, Vector3 P0, Vector3 P1, Vector3 P2, Vector3 P3) // 4 points
+    {
+        Vector3 seekedPoint =
+            -P0 * Mathf.Pow(t, 3) + 3 * P0 * Mathf.Pow(t, 2) - 3 * P0 * t + P0 +
+            3 * P1 * Mathf.Pow(t, 3) - 6 * P1 * Mathf.Pow(t, 2) + 3 * P1 * t +
+            -3 * P2 * Mathf.Pow(t, 3) + 3 * P2 * Mathf.Pow(t, 2) +
+            P3 * Mathf.Pow(t, 3);
+        return seekedPoint;
+    }//для 4 точок
+}
+
 
--- Be free/Assets/Scripts/Camera/CameraControl.cs
+++ Be free/Assets/Scripts/Camera/CameraControl.cs
@@ -7,22 +7,64 @@ public class CameraControl : MonoBehaviour
     [SerializeField] private Transform P0;
     [SerializeField] private Transform P1;
     [SerializeField] private Transform P2;
+    [SerializeField] private Vector3 tempPoint;
     [SerializeField] private Transform lookPoint; 
-    [Range(0f,1f),SerializeField] private float t;
+    [Range(0f,1f),SerializeField] private float t = 0.5f;
     [SerializeField] private Vector3[] cameraLocs;
+    private bool camCheckpoint = false;
+    private bool swiping = false;
+    private bool transferingToNewPoint = false;
+    private int iter = 1;
     private void Start()
     {
         transform.LookAt(lookPoint);
     }
     private void Update()
     {
-        if (Input.GetKey("d"))
+        if (transferingToNewPoint)
         {
-            //P0 = 
-            transform.position = CalculateLerp.CalcLerp(Mathf.Clamp01(t),
-                 P0.position, P1.position, P2.position);
-
+            t += 0.6f * Time.deltaTime;
+            transform.position = CalculateLerp.CalcLerp(t, tempPoint, P0.position);
+            if (t >= 1f) {
+                transferingToNewPoint = false;
+                t = 0f;
+            }
             transform.LookAt(lookPoint);
         }
+        else if (!swiping && Input.GetKey("d"))
+        {
+            swiping = true;
+        }
+        else if (swiping)
+        {
+            t += 0.6f * Time.deltaTime;
+
+            if (camCheckpoint && /*isCamWithinReachToPoint(P1.position)*/t > 0.475f && t < 0.525f)
+            {
+                swiping = false;
+            }
+            if (t >= 1)
+            {
+                camCheckpoint = true;
+                transferingToNewPoint = true;
+                tempPoint = P2.position;
+
+                iter += 3;
+                P0.position = cameraLocs[iter - 1];
+                P1.position = cameraLocs[iter];
+                P2.position = cameraLocs[iter + 1];
+                t = 0f;
+                //t -= 1f;
+            }
+            if (!transferingToNewPoint)
+            {
+                transform.position = CalculateLerp.CalcLerp(Mathf.Clamp01(t),
+                 P0.position, P1.position, P2.position);
+                transform.LookAt(lookPoint);
+            }
+        }
     }
+    //private bool isCamWithinReachToPoint(Vector3 pointPos) {
+    //    return (pointPos - transform.position).magnitude > 0.25f;
+    //}
 }
