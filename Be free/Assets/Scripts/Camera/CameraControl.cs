0e613c825ab4a69c514581213ba013795b7d0063
lentar-1 <danilo232.2004@ukr.net>
2023-02-14 14:18:42 +0200

    camera 3 points

--- Be free/Assets/Scripts/Camera/CameraControl.cs
+++ Be free/Assets/Scripts/Camera/CameraControl.cs
@@ -1,24 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CameraControl : MonoBehaviour
 {
-    [SerializeField] private GameObject cam;
     [SerializeField] private Transform P0;
     [SerializeField] private Transform P1;
     [SerializeField] private Transform P2;
-    [SerializeField] private Transform P3;
     [SerializeField] private Transform lookPoint; 
     [Range(0f,1f),SerializeField] private float t;
-
+    [SerializeField] private Vector3[] cameraLocs;
+    private void Start()
+    {
+        transform.LookAt(lookPoint);
+    }
     private void Update()
     {
-        
-        cam.transform.position = CalculateLerp.CalcLerp(Mathf.Clamp01(t),
-             P0.position, P1.position, P2.position, P3.position);
+        if (Input.GetKey("d"))
+        {
+            //P0 = 
+            transform.position = CalculateLerp.CalcLerp(Mathf.Clamp01(t),
+                 P0.position, P1.position, P2.position);
 
-        cam.transform.LookAt(lookPoint);
+            transform.LookAt(lookPoint);
+        }
     }
 }
